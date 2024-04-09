044c7f30088d7a294631b09cd3479ced05aee5d3
MaxSavi <137710036+MaxSavi@users.noreply.github.com>
2024-04-09 09:49:21 +0300

    Добавьте файлы проекта.

---
+++ WCFch_1Console/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ServiceModel;
+using WCFch_1;
+
+namespace WCFch_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost host = new ServiceHost(typeof(Service1)))
+            {
+                host.Open();
+                Console.WriteLine("Сервис запущен.");
+                Console.WriteLine("Нажмите Enter для остановки сервиса...");
+                Console.ReadLine();
+
+                host.Close();
+            }
+        }
+    }
+}
+
